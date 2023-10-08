b70e82fc912c20084b72291d24a887becb53459f
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-08 22:49:33 +0600

    navLinks dinamically added

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,34 +1,62 @@
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="md:flex justify-between">
-            <div className="text-2xl">This is Navbar</div>
-            <div id="navbar" className="flex gap-2">
-                {/* <NavLink to='/' className={({ isActive, isPending }) => isPending ? 'pending' : isActive ? 'active' : ''}>Home</NavLink> */}
 
-                <NavLink to='/' style={({ isActive, isPending }) => {
-                    return {
-                        // fontWeight: isActive ? "bold" : "",
-                        // color: isPending ? "red" : "black",
-                        color: isPending ? 'blue' : isActive ? 'red' : 'black'
-                    };
-                }}>Home</NavLink>
-                <NavLink to='/login' style={({ isActive, isPending }) => {
-                    return {
-                        // fontWeight: isActive ? "bold" : "",
-                        // color: isPending ? "red" : "black",
-                        color: isPending ? 'blue' : isActive ? 'red' : 'black'
-                    };
-                }}>Login</NavLink>
-                <NavLink to='/register' style={({ isActive, isPending }) => {
-                    return {
-                        // fontWeight: isActive ? "bold" : "",
-                        // color: isPending ? "red" : "black",
-                        color: isPending ? 'blue' : isActive ? 'red' : 'black'
-                    };
-                }}>Register</NavLink>
-            </div>
+    const navLinks = <>
+        <NavLink to='/' style={({ isActive, isPending }) => {
+            return {
+                // fontWeight: isActive ? "bold" : "",
+                // color: isPending ? "red" : "black",
+                color: isPending ? 'blue' : isActive ? 'red' : 'black'
+            };
+        }}>Home</NavLink>
+        <NavLink to='/login' style={({ isActive, isPending }) => {
+            return {
+                // fontWeight: isActive ? "bold" : "",
+                // color: isPending ? "red" : "black",
+                color: isPending ? 'blue' : isActive ? 'red' : 'black'
+            };
+        }}>Login</NavLink>
+        <NavLink to='/register' style={({ isActive, isPending }) => {
+            return {
+                // fontWeight: isActive ? "bold" : "",
+                // color: isPending ? "red" : "black",
+                color: isPending ? 'blue' : isActive ? 'red' : 'black'
+            };
+        }}>Register</NavLink>
+    </>
+
+    return (
+        <>
+            <nav className="md:flex justify-between">
+                <div className="text-2xl">This is Navbar</div>
+                <div id="navbar" className="flex gap-2">
+                    {/* <NavLink to='/' className={({ isActive, isPending }) => isPending ? 'pending' : isActive ? 'active' : ''}>Home</NavLink> */}
+
+                    <NavLink to='/' style={({ isActive, isPending }) => {
+                        return {
+                            // fontWeight: isActive ? "bold" : "",
+                            // color: isPending ? "red" : "black",
+                            color: isPending ? 'blue' : isActive ? 'red' : 'black'
+                        };
+                    }}>Home</NavLink>
+                    <NavLink to='/login' style={({ isActive, isPending }) => {
+                        return {
+                            // fontWeight: isActive ? "bold" : "",
+                            // color: isPending ? "red" : "black",
+                            color: isPending ? 'blue' : isActive ? 'red' : 'black'
+                        };
+                    }}>Login</NavLink>
+                    <NavLink to='/register' style={({ isActive, isPending }) => {
+                        return {
+                            // fontWeight: isActive ? "bold" : "",
+                            // color: isPending ? "red" : "black",
+                            color: isPending ? 'blue' : isActive ? 'red' : 'black'
+                        };
+                    }}>Register</NavLink>
+                </div>
+
+            </nav>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -36,40 +64,21 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {navLinks}
                         </ul>
                     </div>
                     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navLinks}
                     </ul>
                 </div>
                 <div className="navbar-end">
                     <a className="btn">Button</a>
                 </div>
             </div>
-        </nav>
-
+        </>
     );
 };
 
