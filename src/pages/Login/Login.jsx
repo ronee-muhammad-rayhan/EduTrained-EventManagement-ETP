67a6d56b2617a263e9d9e58f34724d71e0b94058
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-09 10:17:39 +0600

    login and register pages added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,40 +1,32 @@
+import { Link } from "react-router-dom";
 import Navbar from "../../components/shared/Navbar/Navbar";
 
 const Login = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <div className="text-3xl">This is Login Page</div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
-                        </form>
-                    </div>
+            <h3 className="text-3xl text-center">Please Login</h3>
+            <form className="md:w-3/4 lg:w-1/2 mx-auto">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" placeholder="email" className="input input-bordered" required />
                 </div>
-            </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Register</button>
+                </div>
+            </form>
+            <p className="text-center">Don&apos;t have an account? <Link className="text-blue-600" to='/register'>Register</Link></p>
         </div>
     );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,19 +5,19 @@ const Register = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h3 className="text-3xl">Please Register</h3>
-            <form>
+            <h3 className="text-3xl text-center">Please Register</h3>
+            <form className="md:w-3/4 lg:w-1/2 mx-auto">
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
-                    <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                    <input type="email" placeholder="email" className="input input-bordered" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                    <input type="password" placeholder="password" className="input input-bordered" required />
                     <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                     </label>
@@ -26,7 +26,7 @@ const Register = () => {
                     <button className="btn btn-primary">Register</button>
                 </div>
             </form>
-            <p>Already have an account? <Link className="text-blue-600" to='/login'>Login</Link></p>
+            <p className="text-center">Already have an account? <Link className="text-blue-600" to='/login'>Login</Link></p>
         </div>
     );
 };
