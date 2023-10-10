ef3ddb3302ebbc58d69c4625913041b5176689f2
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-10 07:07:18 +0600

    assets are added

---
+++ public/data/links.js
@@ -0,0 +1,7 @@
+// https://i.ibb.co/BjYt4z9/banner.jpg
+// https://i.ibb.co/MS7rwHr/banner2.jpg
+// https://i.ibb.co/f016DXg/banner-3.jpg
+// https://i.ibb.co/3mPD420/banner-4.jpg
+// https://i.ibb.co/YX7M8b9/banner-5.jpg
+
+// https://i.ibb.co/f8YwxyC/logo.png
