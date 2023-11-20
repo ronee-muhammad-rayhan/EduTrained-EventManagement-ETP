a3c1d2e721192912e9119ee38690776c601b0e31
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-20 18:33:25 +0600

    service details page qualified

--- src/pages/Services/ServiceDetails.jsx
+++ src/pages/Services/ServiceDetails.jsx
@@ -11,6 +11,34 @@ const ServiceDetails = () => {
     return (
         <div>
 
+
+            <section>
+                <div className="dark:bg-violet-400">
+                    <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
+                        <h1 className="text-5xl font-bold leadi sm:text-6xl xl:max-w-3xl dark:text-gray-900">{name}</h1>
+                        <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">{shortDescription}</p>
+                        <div className="flex flex-wrap justify-center">
+                            <button type="button" className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-gray-800 dark:text-gray-50">Get started</button>
+                            <button type="button" className="px-8 py-3 m-2 text-lg border rounded dark:border-gray-700 dark:text-gray-900">Learn more</button>
+                        </div>
+                    </div>
+                </div>
+                <img src={image} alt="" className="w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 dark:bg-gray-500" />
+            </section>
+            {/* <section>
+                <div className="dark:bg-violet-400">
+                    <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
+                        <h1 className="text-5xl font-bold leadi sm:text-6xl xl:max-w-3xl dark:text-gray-900">Provident blanditiis cum exercitationem</h1>
+                        <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+                        <div className="flex flex-wrap justify-center">
+                            <button type="button" className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-gray-800 dark:text-gray-50">Get started</button>
+                            <button type="button" className="px-8 py-3 m-2 text-lg border rounded dark:border-gray-700 dark:text-gray-900">Learn more</button>
+                        </div>
+                    </div>
+                </div>
+                <img src="https://source.unsplash.com/random/480x320" alt="" className="w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 dark:bg-gray-500" />
+            </section> */}
+
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 dark:bg-gray-800 dark:text-gray-100">
                 <h2 className="mb-8 text-4xl font-bold leadi text-center">Key Features</h2>
                 <ul className="grid gap-3 md:grid-cols-2 lg:grid-cols-4">
