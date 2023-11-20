f459d2432d211aef14f60bf8e1b9ba7d6b8c1165
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-20 19:05:59 +0600

    prevent reload issue

--- src/routes/ProtectedRoutes/PrivateRoutes.jsx
+++ src/routes/ProtectedRoutes/PrivateRoutes.jsx
@@ -6,7 +6,15 @@ const PrivateRoutes = ({
     redirectPath = '/login',
     children,
   }) => {
-    const {user}=useAuth();
+    const {user, loading}=useAuth();
+    if(loading){
+      return <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+    //   return <div className="flex items-center justify-center space-x-2">
+    //   <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
+    //   <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
+    //   <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
+    // </div>
+    }
     if (!user) {
       return <Navigate to={redirectPath} replace />;
     }
