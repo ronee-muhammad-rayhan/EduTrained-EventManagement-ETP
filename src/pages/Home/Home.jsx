33ce522f4c6ba210f1f7a2d5373f3c0d42410486
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-21 18:54:25 +0600

    dynamic testimonials

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -73,6 +73,7 @@ const Home = () => {
                     <h3 className="text-center text-3xl font-bold py-8">Our services</h3>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-3 mx-auto w-full">
                         {
+                            // services.services?.slice(3, 6).map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)
                             services.services?.map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)
                         }
                     </div>
@@ -88,26 +89,30 @@ const Home = () => {
                         <h1 className="p-4 text-4xl font-semibold leadi text-center">What our customers are saying about us</h1>
                     </div>
                     <div className="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
-                        <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
-                            <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
-                                <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
-                                        <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-                                        <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                                    </svg>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus quibusdam, eligendi exercitationem molestias possimus facere.
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
-                                        <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-                                        <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                                    </svg>
-                                </p>
-                            </div>
-                            <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
-                                <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-700" />
-                                <p className="text-xl font-semibold leadi">Distinctio Animi</p>
-                                <p className="text-sm uppercase">Aliquam illum</p>
-                            </div>
-                        </div>
-                        <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+                        {
+                            tempts?.slice(3, 8).map((testimonial, index) => <div key={index} className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+                                <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+                                    <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
+                                            <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                            <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                                        </svg>{testimonial?.testimonial}
+                                        {/* </svg>{tempts[3].testimonial} */}
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
+                                            <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                            <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                                        </svg>
+                                    </p>
+                                </div>
+                                <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
+                                    <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-700" />
+                                    <p className="text-xl font-semibold leadi">{testimonial?.clientName}</p>
+                                    {/* <p className="text-xl font-semibold leadi">{tempts[3]?.clientName}</p> */}
+                                    <p className="text-sm uppercase">Aliquam illum</p>
+                                </div>
+                            </div>)
+                        }
+                        {/* <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
                             <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
                                 <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
@@ -163,7 +168,7 @@ const Home = () => {
                                 <p className="text-xl font-semibold leadi">Distinctio Animi</p>
                                 <p className="text-sm uppercase">Aliquam illum</p>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </section>
             </div>
